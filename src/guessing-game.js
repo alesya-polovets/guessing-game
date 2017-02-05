319543762ba70fe5a19ccf7ed826949ba2fbc290
Alesya Polovets <alesya.polovets@gmail.com>
2017-02-05 22:03:30 +0300

    Implemented methods for guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+        this.min = null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.ceil(this.min + (this.max - this.min) / 2);
     }
 
-    lower() {
+    lower() {this.max = this.guess();}
 
-    }
-
-    greater() {
-
-    }
+    greater() {this.min = this.guess();}
 }
 
 module.exports = GuessingGame;
